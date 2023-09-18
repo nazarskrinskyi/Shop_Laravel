f9affdcc21b5493f829080ecde34f1ddeaf6d01b
Nazar <nazarskrinskyi@gmail.com>
2023-09-18 17:06:47 +0300

    little updates and bug fix

---
+++ app/Http/Requests/Api/Order/IndexRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Api\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class IndexRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'products' => 'nullable|array',
+            'name' => 'nullable|string',
+            'email' => 'required|email',
+            'address' => 'nullable|string',
+            'total_price' => 'nullable|integer',
+        ];
+    }
+}
